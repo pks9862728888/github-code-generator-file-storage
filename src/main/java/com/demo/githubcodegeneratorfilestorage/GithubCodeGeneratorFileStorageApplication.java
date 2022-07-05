f2d09cce3c83cb2ffaa64ab231b63b0c07cf8fb8
hackedlings <pks9862728888@gmail.com>
2022-07-05 14:18:45 +0530

    Initial commit

---
+++ src/main/java/com/demo/githubcodegeneratorfilestorage/GithubCodeGeneratorFileStorageApplication.java
@@ -0,0 +1,13 @@
+package com.demo.githubcodegeneratorfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCodeGeneratorFileStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCodeGeneratorFileStorageApplication.class, args);
+	}
+
+}
